4f938bc9987ce724aa35e35344c56505b2d6a404
Elango Cheran <elango.cheran@gmail.com>
2011-11-06 10:38:06 -0800

    adding files created by leiningen for the lein project to represent the complication of wfeditor

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject wfeditor "1.0.0-SNAPSHOT"
+  :description "Workflow Welder: a user interface for managing the execution of complex scientific computation on various compute infrastructures"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 ;; clojure-contrib 1.2.1 doesn't exist, 1.2.0 is last on 1.2x branch
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :disable-deps-clean true
+  :aot [core]
+  :jvm-opts ["-XstartOnFirstThread"])
