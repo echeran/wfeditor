3c99cd6edbfa940c662633af546beec1cad8fbe0
Elango Cheran <elango.cheran@gmail.com>
2011-11-06 23:59:02 -0800

    figured out the right dependencies for JFace on Mac OS X, as reflected in the clojure-rlwrap script; the JFace example in main.clj compiles and works

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
                  ;; clojure-contrib 1.2.1 doesn't exist, 1.2.0 is last on 1.2x branch
                  [org.clojure/clojure-contrib "1.2.0"]]
   :disable-deps-clean true
-  :aot [wfeditor.core]
+  :aot [wfeditor.main]
   :jvm-opts ["-XstartOnFirstThread"])
--- src/wfeditor/main.clj
+++ src/wfeditor/main.clj
@@ -1,30 +1,81 @@
 (ns wfeditor.main)
 
-(import 'org.eclipse.swt.SWT
-       'org.eclipse.swt.graphics.Color
-       'org.eclipse.swt.layout.FillLayout
-       'org.eclipse.swt.widgets.Display
-       'org.eclipse.swt.widgets.Label
-       'org.eclipse.swt.widgets.Shell)
+
+;; (import 'org.eclipse.swt.SWT
+;;        'org.eclipse.swt.graphics.Color
+;;        'org.eclipse.swt.layout.FillLayout
+;;        'org.eclipse.swt.widgets.Display
+;;        'org.eclipse.swt.widgets.Label
+;;        'org.eclipse.swt.widgets.Shell)
+
+;; (let [display (Display.)
+;;      shell (Shell. display)
+;;      label (Label. shell SWT/CENTER)
+;;      red (Color. nil 255 0 0)]
+;; (doto shell
+;;  (.setText "Hello World")
+;;  (.setBounds 100 100 200 50)
+;;  (.setLayout (FillLayout.)))
+
+;; (doto label
+;;  (.setText "Hello World")
+;;  (.setForeground red))
+
+;; (. shell open)
+;; (while (not (. shell isDisposed))
+;;  (if (not (. display readAndDispatch)) (. display sleep)))
+
+;; (. red dispose)
+;; (. display dispose))
+
+
+
+(defstruct person :first :last :age :child)
+
+(def example
+     (map #(apply (partial struct person)  %)
+           (partition 4 ["Dan" "Rubel" 38 [(struct person "Beth" "Rubel" 8)
+                                           (struct person "David" "Rubel" 3)]
+                         "Eric" "Clayberg" 39 [(struct person "Lauren" "Clayberg" 6)
+                                               (struct person "Lee" "Clayberg" 4)]
+                         "Mike" "Taylor" 52 nil])))
+
+(import '(org.eclipse.swt.widgets
+          Display Shell Table TableColumn)
+        'org.eclipse.swt.SWT
+        'org.eclipse.swt.layout.FillLayout
+        '(org.eclipse.jface.viewers
+          ArrayContentProvider TableViewer LabelProvider ITableLabelProvider))
 
 (let [display (Display.)
-     shell (Shell. display)
-     label (Label. shell SWT/CENTER)
-     red (Color. nil 255 0 0)]
-(doto shell
- (.setText "Hello World")
- (.setBounds 100 100 200 50)
- (.setLayout (FillLayout.)))
+      shell (doto (Shell. display)
+              (.setText "Table Viewer Example")
+              (.setBounds 100 100 325 200)
+              (.setLayout (FillLayout.)))
+      tableViewer (TableViewer.  shell (bit-or SWT/SINGLE SWT/FULL_SELECTION))
+      table (. tableViewer getTable)]
 
-(doto label
- (.setText "Hello World")
- (.setForeground red))
+  (. table setHeaderVisible true)
+  (. table setLinesVisible true)
 
-(. shell open)
-(while (not (. shell isDisposed))
- (if (not (. display readAndDispatch)) (. display sleep)))
+  (doall (map #(doto (TableColumn. table %1) (.setText %2) (.setWidth %3))
+                [SWT/LEFT SWT/LEFT SWT/CENTER SWT/CENTER]
+               ["First Name" "Last Name" "Age" "Num Children"]
+                [100 100 35 75]))
 
-(. red dispose)
-(. display dispose))
+  (. tableViewer setLabelProvider
+                 (proxy [LabelProvider ITableLabelProvider] []
+                    (getColumnImage [_1 _2] nil)
+                   (getColumnText [element idx]
+                     (str (cond (= idx 0) (:first element)
+                                 (= idx 1) (:last element)
+                                 (= idx 2) (:age element)
+                                 (= idx 3) (count (:child element)))))))
 
-(println "Hallo, Welt!")
+  (. tableViewer setContentProvider (ArrayContentProvider.))
+  (. tableViewer setInput (to-array example))
+
+  (. shell open)
+  (while (not (. shell isDisposed))
+    (if (not (. display readAndDispatch)) (. display sleep)))
+  (. display dispose))
