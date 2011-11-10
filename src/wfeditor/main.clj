3812506f904f7500a10d49eabb171fe6285c132a
Elango Cheran <elango.cheran@gmail.com>
2011-11-09 16:08:36 -0800

    have an almost-but-not-quite working version of the JFace example application ported to Clojure using Clojure's proxy function; for some reason, it's not working, so I think I will try implementing JFace using Clojure's gen-class function

--- src/wfeditor/main.clj
+++ src/wfeditor/main.clj
@@ -30,52 +30,122 @@
 
 
 
-(defstruct person :first :last :age :child)
 
-(def example
-     (map #(apply (partial struct person)  %)
-           (partition 4 ["Dan" "Rubel" 38 [(struct person "Beth" "Rubel" 8)
-                                           (struct person "David" "Rubel" 3)]
-                         "Eric" "Clayberg" 39 [(struct person "Lauren" "Clayberg" 6)
-                                               (struct person "Lee" "Clayberg" 4)]
-                         "Mike" "Taylor" 52 nil])))
+;; (defstruct person :first :last :age :child)
 
-(import '(org.eclipse.swt.widgets
-          Display Shell Table TableColumn)
+;; (def example
+;;      (map #(apply (partial struct person)  %)
+;;            (partition 4 ["Dan" "Rubel" 38 [(struct person "Beth" "Rubel" 8)
+;;                                            (struct person "David" "Rubel" 3)]
+;;                          "Eric" "Clayberg" 39 [(struct person "Lauren" "Clayberg" 6)
+;;                                                (struct person "Lee" "Clayberg" 4)]
+;;                          "Mike" "Taylor" 52 nil])))
+
+;; (import '(org.eclipse.swt.widgets
+;;           Display Shell Table TableColumn)
+;;         'org.eclipse.swt.SWT
+;;         'org.eclipse.swt.layout.FillLayout
+;;         '(org.eclipse.jface.viewers
+;;           ArrayContentProvider TableViewer LabelProvider ITableLabelProvider))
+
+;; (let [display (Display.)
+;;       shell (doto (Shell. display)
+;;               (.setText "Table Viewer Example")
+;;               (.setBounds 100 100 325 200)
+;;               (.setLayout (FillLayout.)))
+;;       tableViewer (TableViewer.  shell (bit-or SWT/SINGLE SWT/FULL_SELECTION))
+;;       table (. tableViewer getTable)]
+
+;;   (. table setHeaderVisible true)
+;;   (. table setLinesVisible true)
+
+;;   (doall (map #(doto (TableColumn. table %1) (.setText %2) (.setWidth %3))
+;;                 [SWT/LEFT SWT/LEFT SWT/CENTER SWT/CENTER]
+;;                ["First Name" "Last Name" "Age" "Num Children"]
+;;                 [100 100 35 75]))
+
+;;   (. tableViewer setLabelProvider
+;;                  (proxy [LabelProvider ITableLabelProvider] []
+;;                     (getColumnImage [_1 _2] nil)
+;;                    (getColumnText [element idx]
+;;                      (str (cond (= idx 0) (:first element)
+;;                                  (= idx 1) (:last element)
+;;                                  (= idx 2) (:age element)
+;;                                  (= idx 3) (count (:child element)))))))
+
+;;   (. tableViewer setContentProvider (ArrayContentProvider.))
+;;   (. tableViewer setInput (to-array example))
+
+;;   (. shell open)
+;;   (while (not (. shell isDisposed))
+;;     (if (not (. display readAndDispatch)) (. display sleep)))
+;;   (. display dispose))
+
+
+
+;; (import 'org.eclipse.jface.window.ApplicationWindow
+;;         'org.eclipse.swt.SWT
+;;         'org.eclipse.swt.layout.FillLayout
+;;         '(org.eclipse.swt.widgets Display Shell Label))
+
+;; (defn set-label-props [clientarea label]
+;;   (doto label
+;;     (.setText "Hello, World")
+;;     (.setBounds clientarea)))
+
+;; (let [display (Display.)
+;;       shell (Shell.)
+;;       label (Label. shell  SWT/CENTER)
+;;       label2 (Label. shell  SWT/CENTER)
+;;       clientarea (. shell getClientArea)]
+;;   ;; (doto label
+;;   ;;   (.setText "Hello, World")
+;;   ;;   (.setBounds clientarea))
+;;   (do
+;;     (set-label-props clientarea label)
+;;     (set-label-props clientarea label2)
+;;     )
+;;   (doto shell
+;;     (.setText "Hello World")
+;;     (.setBounds 100 100 300 150)
+;;     (.setLayout (FillLayout.)))
+
+;;   (. shell open)
+;;   (while (not (. shell isDisposed))
+;;     (if (not (. display readAndDispatch)) (. display sleep)))
+;;   (. display dispose))
+
+
+
+
+
+(import 'org.eclipse.jface.window.ApplicationWindow
         'org.eclipse.swt.SWT
         'org.eclipse.swt.layout.FillLayout
-        '(org.eclipse.jface.viewers
-          ArrayContentProvider TableViewer LabelProvider ITableLabelProvider))
+        '(org.eclipse.swt.widgets Display Shell Label))
 
-(let [display (Display.)
-      shell (doto (Shell. display)
-              (.setText "Table Viewer Example")
-              (.setBounds 100 100 325 200)
-              (.setLayout (FillLayout.)))
-      tableViewer (TableViewer.  shell (bit-or SWT/SINGLE SWT/FULL_SELECTION))
-      table (. tableViewer getTable)]
+(defn proxy-handler []
+  (proxy [ApplicationWindow]
+      [nil]  ;; ApplicationWindow constructor (i.e., the call to
+    ;; super).  note, not using proxy-super method, but good to know about
+      (run []
+        ((. this setBlockOnOpen) true)
+        (. this open)
+        (.. Display getCurrent dispose))
+      (createContents [parent]
+        (let [label2 (Label. parent  SWT/CENTER)]
+          (doto label2
+            (.setText "Hello, World"))))))
 
-  (. table setHeaderVisible true)
-  (. table setLinesVisible true)
+;; (defn createContents [parent]
+;;   (let [label2 (Label. parent  SWT/CENTER)]
+;;     (doto label2
+;;       (.setText "Hello, World"))))
 
-  (doall (map #(doto (TableColumn. table %1) (.setText %2) (.setWidth %3))
-                [SWT/LEFT SWT/LEFT SWT/CENTER SWT/CENTER]
-               ["First Name" "Last Name" "Age" "Num Children"]
-                [100 100 35 75]))
+(let [app-win (proxy-handler)]
 
-  (. tableViewer setLabelProvider
-                 (proxy [LabelProvider ITableLabelProvider] []
-                    (getColumnImage [_1 _2] nil)
-                   (getColumnText [element idx]
-                     (str (cond (= idx 0) (:first element)
-                                 (= idx 1) (:last element)
-                                 (= idx 2) (:age element)
-                                 (= idx 3) (count (:child element)))))))
-
-  (. tableViewer setContentProvider (ArrayContentProvider.))
-  (. tableViewer setInput (to-array example))
-
-  (. shell open)
-  (while (not (. shell isDisposed))
-    (if (not (. display readAndDispatch)) (. display sleep)))
-  (. display dispose))
+  ;; (. shell open)
+  ;; (while (not (. shell isDisposed))
+  ;;   (if (not (. display readAndDispatch)) (. display sleep)))
+  ;; (. display dispose)
+  (.run app-win))
