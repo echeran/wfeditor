ad3a4cda8cdb6c5e319af5574a3947ac379cd0f2
Elango Cheran <elango.cheran@gmail.com>
2011-11-17 18:27:18 -0800

    adding code to create sash for main editor and do proper FormLayout attachments, but no code yet to make the sash stick

--- src/wfeditor/main.clj
+++ src/wfeditor/main.clj
@@ -4,7 +4,7 @@
   (:import
   org.eclipse.jface.window.ApplicationWindow
   org.eclipse.swt.SWT
-  org.eclipse.swt.layout.FillLayout
+  (org.eclipse.swt.layout FillLayout FormLayout FormData FormAttachment)
   (org.eclipse.swt.widgets Display Shell Label Button Sash Composite)))
 
 (defmacro new-widget [widget-class parent style]
@@ -18,7 +18,7 @@
   (dorun (map #(.setText (Button. parent style) %1) names))
   )
 
-(defn ui-editor-left [parent]
+(defn ui-editor-left-create [parent]
   (let [label2 (Label. parent  SWT/CENTER)]
     (do
       (.setLayout parent (FillLayout. SWT/VERTICAL)))
@@ -27,24 +27,83 @@
       (.setBounds (.getClientArea parent)))
     (create-widgets-with-names parent Button SWT/PUSH ["one" "two" "three"])))
 
-(defn ui-editor-right [parent]
+(defn ui-editor-right-create [parent]
   (let [label1 (new Label parent SWT/RIGHT)]
     (do
-      (.setLayout parent (FillLayout. SWT/HORIZONTAL))
+      (.setLayout parent (FillLayout. SWT/VERTICAL))
       (create-widgets-with-names parent Button SWT/RADIO ["Radio 1" "Radio 2" "Radio 3"])
       (create-widgets-with-names parent Button SWT/TOGGLE ["Tog 1" "Tog 2" "Tog 3"])
       (create-widgets-with-names parent Button SWT/CHECK [ "Check one" "...two" "...three"]))
     (doto label1
       (.setText "hw 2.1"))))
 
-(defn ui-editor [parent]
-  (let [comp-left (Composite. parent SWT/NONE)
-        sash (new Sash parent SWT/VERTICAL)
-        comp-right (Composite. parent SWT/NONE)]
+;; assume FormLayout of the parent widget to which the returned sash
+;; will be attached
+(defn ui-editor-sash [parent]
+  (let [sash (Sash. parent SWT/VERTICAL)
+        data (FormData.)]
     (do
-      (.setLayout parent (FillLayout. SWT/HORIZONTAL))
-      (ui-editor-left comp-left)
-      (ui-editor-right comp-right))))
+      (set! (. data top) (FormAttachment. 0 0))
+      (set! (. data bottom) (FormAttachment. 100 0))
+      (set! (. data left) (FormAttachment. 50 0))
+      (.setLayoutData sash data))
+    sash))
+
+;; (defn create-sash [parent left right]
+;;   (let [sash (Sash. parent SWT/VERTICAL)
+;;         sash-fdata (FormData.)
+;;         left-fdata (FormData.)
+;;         right-fdata (FormData.)]
+;;     (do
+;;       (set! (. sash-fdata top) (FormAttachment. 0 0))
+;;       (set! (. sash-fdata bottom) (FormAttachment. 100 0))
+;;       (set! (. sash-fdata left) (FormAttachment. 50 0))
+;;       (.setLayoutData sash sash-fdata))
+;;     (do
+;;       (set! (. left-fdata top) (FormAttachment. 0 0))
+;;       (set! (. left-fdata bottom) (FormAttachment. 100 0))
+;;       (set! (. left-fdata left) (FormAttachment. 0 0))
+;;       (set! (. left-fdata right) (FormAttachment. sash 0))
+;;       (.setLayoutData left left-fdata))
+;;     (do
+;;       (set! (. right-fdata top) (FormAttachment. 0 0))
+;;       (set! (. right-fdata bottom) (FormAttachment. 100 0))
+;;       (set! (. right-fdata left) (FormAttachment. sash 0))
+;;       (set! (. right-fdata right) (FormAttachment. 100 0))
+;;       (.setLayoutData right right-fdata))
+;;     sash))
+
+(defn ui-editor-create [parent]
+  (let [
+        sash (Sash. parent SWT/VERTICAL)
+        comp-left (Composite. parent SWT/NONE)
+        comp-right (Composite. parent SWT/NONE)
+        ;; sash (create-sash parent comp-left comp-right)
+        sash-fdata (FormData.)
+        comp-left-fdata (FormData.)
+        comp-right-fdata (FormData.)
+        ]
+    (do
+      (.setLayout parent (FormLayout.))
+      (ui-editor-left-create comp-left)
+      (ui-editor-right-create comp-right))
+    (do
+      (set! (. sash-fdata top) (FormAttachment. 0 0))
+      (set! (. sash-fdata bottom) (FormAttachment. 100 0))
+      (set! (. sash-fdata left) (FormAttachment. 50 0))
+      (.setLayoutData sash sash-fdata))
+    (do
+      (set! (. comp-left-fdata top) (FormAttachment. 0 0))
+      (set! (. comp-left-fdata bottom) (FormAttachment. 100 0))
+      (set! (. comp-left-fdata left) (FormAttachment. 0 0))
+      (set! (. comp-left-fdata right) (FormAttachment. sash 0))
+      (.setLayoutData comp-left comp-left-fdata))
+    (do
+      (set! (. comp-right-fdata top) (FormAttachment. 0 0))
+      (set! (. comp-right-fdata bottom) (FormAttachment. 100 0))
+      (set! (. comp-right-fdata left) (FormAttachment. sash 0))
+      (set! (. comp-right-fdata right) (FormAttachment. 100 0))
+      (.setLayoutData comp-right comp-right-fdata))))
 
 ;; JFace way of creating a window is to subclass ApplicationWindow and
 ;; override createContents
@@ -61,7 +120,7 @@
     ;; should be applied to the instance of the extended class
     ;; returned by proxy
     (createContents [parent]
-      (ui-editor parent))))
+      (ui-editor-create parent))))
 
 ;; The JFace idiomatic way of displaying a window.  As it seems, using
 ;; the "plain SWT" idiom for displaying a window doesn't work for
