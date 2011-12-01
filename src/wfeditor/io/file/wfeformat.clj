0884ebf30da46eb5aa6e7a3cffae42f21f694b25
Elango Cheran <elango.cheran@gmail.com>
2011-12-01 10:24:17 -0800

    adding beginnings of file format reading and writing namespace

---
+++ src/wfeditor/io/file/wfeformat.clj
@@ -0,0 +1,7 @@
+(ns wfeditor.io.file.wfformat)
+
+(defn file-seq
+  "return a lazy sequence of strings representing the contents of the file format representing the file"
+  [filename]
+  ;; TODO: implement this and rest of file
+  nil)
