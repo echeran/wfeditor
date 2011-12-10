4ae58a8071254174c06b961826afcff77488ad3e
Elango Cheran <elango.cheran@gmail.com>
2011-12-10 13:40:30 -0800

    updates to testing draw2d code

--- src/wfeditor/ui/gui/core.clj
+++ src/wfeditor/ui/gui/core.clj
@@ -8,7 +8,7 @@
    (org.eclipse.swt.layout FillLayout FormLayout FormData FormAttachment GridLayout GridData)
    (org.eclipse.swt.widgets Display Shell Label Button Sash Composite Canvas)
    (org.eclipse.swt.events SelectionEvent SelectionAdapter)
-   (org.eclipse.draw2d Figure XYLayout ColorConstants LightweightSystem RectangleFigure ToolbarLayout)
+   (org.eclipse.draw2d Figure XYLayout ColorConstants LightweightSystem RectangleFigure ToolbarLayout PolygonShape PolylineConnection ChopboxAnchor)
    (org.eclipse.draw2d.geometry Rectangle Point)
    ;; (org.eclipse.swt.graphics Rectangle Point) have to comment this
    ;; out since it conflicts with the draw2d import statements
@@ -24,6 +24,28 @@
 
 (defn create-buttons-with-names [parent  style names]
   (dorun (map #(.setText (Button. parent style) %1) names)))
+
+(defn connect
+  "create and return a line segment that connects two figures to each other on the line connecting their respective centers"
+  [fig1 fig2]
+  (let [conn (PolylineConnection.)]
+    (doto conn
+      (.setSourceAnchor (ChopboxAnchor. fig1))
+      (.setTargetAnchor (ChopboxAnchor. fig2)))))
+
+(defn create-marriage-figure
+  "create and return a figure representing the marriage between two people and the relationship to a 3rd person, their child"
+  []
+  (let [r (Rectangle. 0 0 50 50)
+        polygon-shape (PolygonShape.)]
+    (.setStart polygon-shape (.getTop r))
+    (dorun (map (fn [point] (.addPoint polygon-shape point)) [(.getTop r) (.getTop r) (.getLeft r) (.getBottom r) (.getRight r) (.getTop r)]))
+    (.setEnd polygon-shape (.getTop r))
+    (doto polygon-shape
+      (.setFill true)
+      (.setBackgroundColor ColorConstants/lightGray)
+      (.setPreferredSize (.getSize r))))
+  )
 
 (defn create-person-figure
   "create a figure representing a person"
@@ -44,13 +66,18 @@
         lws (LightweightSystem. canvas)
         andy (create-person-figure "Andy")
         betty (create-person-figure "Betty")
-        carl (create-person-figure "Carl")]
+        carl (create-person-figure "Carl")
+        marriage (create-marriage-figure)]
     (doto root
       (.setFont (.getFont parent))
       (.setLayoutManager layout)
       (.add andy)
       (.add betty)
-      (.add carl))
+      (.add carl)
+      (.add marriage (Rectangle. (Point. 145 35) (.getPreferredSize marriage)))
+      (.add (connect andy marriage))
+      (.add (connect betty marriage))
+      (.add (connect carl marriage)))
     (doto layout
       (.setConstraint andy (Rectangle. (Point. 10 10) (.getPreferredSize andy)))
       (.setConstraint betty (Rectangle. (Point. 230 10) (.getPreferredSize betty)))
