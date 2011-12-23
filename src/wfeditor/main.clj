1a362880eea05fd351523cc6d7d41dec390f6bc7
Elango Cheran <elango.cheran@gmail.com>
2011-12-23 12:16:34 -0800

    added code to allow the user to provide a command-line graph edge description

--- src/wfeditor/main.clj
+++ src/wfeditor/main.clj
@@ -3,9 +3,18 @@
   ;; this allows a standalone uberjar to be run by Java
   (:gen-class)
   (:require clojure.tools.cli)
-  (:require wfeditor.ui.gui.core)
+  (:require wfeditor.ui.gui.core
+            [wfeditor.ui.gui.zest.graph :as zgraph])
   (:import
    org.eclipse.swt.widgets.Display))
+
+(defn handle-common-args
+  "handle arguments and options that are common for the entire program, whether the program is eventually run in command-line mode, gui mode, or etc."
+  [options args]
+  (if (:graph options)
+    (let [new-graph (:graph options)
+          new-node-adj-map (zgraph/node-adj-map (:graph options))] 
+      (dosync (alter zgraph/g assoc :neighbors new-node-adj-map)))))
 
 (defn ui-create
   "The entry point to building the entire UI.  Uses a JFace idiom to do this, so UI code comes from an extended (proxied) ApplicationWindow.
@@ -21,8 +30,11 @@ TODO: handle options and args coming in from the CLI"
   ;; subclass constructor, that should go somewhere here, where the
   ;; instance is actually being returned and manipulated
   (let [app-win (wfeditor.ui.gui.core/app-win-proxy)]
+
     ;; TODO: use CLI options and args to modify the UI here, which is
-    ;; after its instantiation
+    ;; after its instantiation 
+    (handle-common-args options args)
+    
     (. app-win setBlockOnOpen true)
     (. app-win open)
     (when-let [display (. Display getCurrent)]
@@ -39,7 +51,8 @@ TODO: handle options and args coming in from the CLI"
   [user-args]
   (let [[options arguments banner] (clojure.tools.cli/cli user-args
                                         ["-g" "--[no-]gui" "Run the GUI frontend with the program" :default true :flag true]
-                                        ["-h" "--help" "Display the command-line help statement" :default false :flag true])]
+                                        ["-h" "--help" "Display the command-line help statement" :default false :flag true]
+                                        ["--graph" "An initial job dependency graph as a map of int -> nested vector of ints(Ex:  {0 [1 4], 1 [3], 2 [1]})" :default nil :parse-fn load-string])]
     (when (:help options)
       (println banner)
       (System/exit 0))
