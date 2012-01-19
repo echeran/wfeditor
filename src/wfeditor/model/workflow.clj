38484f3e5196741edc6510f8e5a9ead558cbb864
Elango Cheran <elango.cheran@gmail.com>
2012-01-18 16:20:41 -0800

    removed the 'deps' field from the Job type since it was redundant (this is instead represented via the Graph type)

--- src/wfeditor/model/workflow.clj
+++ src/wfeditor/model/workflow.clj
@@ -18,7 +18,7 @@
 ;; proposed unique identifier for Job = id
 ;; for a listing of required and optional arguments, see new-job-fn
 ;; the deps of a job is implicitly stored in the global Graph object g
-(defrecord Job [id name desc prog-name prog-ver prog-exec-loc prog-exec-ver prog-args prog-opts std-out-file std-err-file deps])
+(defrecord Job [id name desc prog-name prog-ver prog-exec-loc prog-exec-ver prog-args prog-opts std-out-file std-err-file])
 
 ;; replacement for the defstruct declaration of graphs in
 ;; clojure.contrib.graph
@@ -72,8 +72,8 @@ when supplying arguments to the function, the following are required
 name, prog-exec-loc, prog-args prog-opts
 the following are optional:
 id desc prog-name prog-ver prog-exec-ver std-out-file std-err-file deps"
-  [name prog-exec-loc prog-args prog-opts & {:keys [id desc prog-name prog-ver prog-exec-ver std-out-file std-err-file deps] :or {id nil desc nil prog-name nil prog-ver nil prog-exec-ver nil std-out-file nil std-err-file nil deps []}}]
-  (Job. id name desc prog-name prog-ver prog-exec-loc prog-exec-ver prog-args prog-opts std-out-file std-err-file deps))
+  [name prog-exec-loc prog-args prog-opts & {:keys [id desc prog-name prog-ver prog-exec-ver std-out-file std-err-file] :or {id nil desc nil prog-name nil prog-ver nil prog-exec-ver nil std-out-file nil std-err-file nil}}]
+  (Job. id name desc prog-name prog-ver prog-exec-loc prog-exec-ver prog-args prog-opts std-out-file std-err-file))
 
 (defn new-graph-fn
   "return a new graph type"
