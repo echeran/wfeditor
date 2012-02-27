4b5bf064571f86e20e0abc186f1dc48b28e35140
Elango Cheran <elango.cheran@gmail.com>
2012-02-27 14:56:50 -0800

    minor cleanup of Noir server Clojure files

--- src/wfeditor/io/relay/server_views/welcome.clj
+++ src/wfeditor/io/relay/server_views/welcome.clj
@@ -8,71 +8,17 @@
         [hiccup form-helpers page-helpers]
         ))
 
-;; (defpage "/welcome" []
-;;          (common/layout
-;;            [:p "Welcome to WFE server test"]))
-
-;; ;; A very simple page definition that maps to the root of your site.
-;; (defpage "/" []
-;;   "hello")
-
 (defpage [:get "/"] [] "This is a get") ;; same as (defpage "/" [] ..)
 (defpage [:post "/"] [] "This is a post")
 (defpage [:put "/"] [] "This is a put")
 (defpage [:any "/"] [] "This is any request type")
 
-;; (defpage [:post "/login"] {:keys [username password]}
-;;   (str "You tried to login as " username " with the password " password))
-
-;; (defpage "/login" []
-;;   (str "You need to login?"))
-
 (defpage "/error" []
   {:status 500
    :body "Oh no! An error has occurred"})
 
-;; (pre-route "/admin/*" {}
-;;            (when-not (users/admin?)
-;;              (resp/redirect "/login")))
+(defpage [:get "/workflow"] []
+  "Request the status of a workflow")
 
-
-;; (defpartial layout [& content]
-;;   (html5
-;;    [:head
-;;     [:title "Forms"]]
-;;    [:body
-;;     content]))
-
-;; (defpartial user-fields [{:keys [firstname lastname]}]
-;;   (label "firstname" "First name: ")
-;;   (text-field "firstname" firstname)
-;;   (label "lastname" "Last name: ")
-;;   (text-field "lastname" lastname))
-
-;; (defpage "/user/add" {:as user}
-;;   (layout
-;;    (form-to [:post "/user/add"]
-;;             (user-fields user)
-;;             (submit-button "Add user"))))
-
-;; (defn valid? [{:keys [firstname lastname]}]
-;;   true)
-
-;; (defpage [:post "/user/add"] {:as user}
-;;   (if (valid? user)
-;;     (layout
-;;      [:p "User added!"])
-;;     (render "/user/add" user)))
-
-;; (defn valid? [{:keys [firstname lastname]}]
-;;   (vali/rule (vali/min-length? firstname 5)
-;;              [:firstname "Your first name must have more than 5 letters."])
-;;   (vali/rule (vali/has-value? lastname)
-;;              [:lastname "You must have a last name"])
-;;   (not (vali/errors? :lastname :firstname)))
-
-;; (defpage [:post "/user/add"] {:as user}
-;;   (if (valid? user)
-;;     (layout
-;;      [:p "User added!"])
-;;     (render "/user/add" user)))
+(defpage [:post "/workflow"] []
+  "Create a new instance of a workflow")
