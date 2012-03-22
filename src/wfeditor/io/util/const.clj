d785468e2c86804453bfec37124a945903c2b029
Elango Cheran <elango.cheran@gmail.com>
2012-03-22 13:40:07 -0700

    this file was needed by the commit 2 commits ago but left out

---
+++ src/wfeditor/io/util/const.clj
@@ -0,0 +1,13 @@
+(ns wfeditor.io.util.const)
+;; a class whose purpose is to store constants
+;; hopefully, this will help prevent cyclical namespace
+;; requirements/dependencies
+
+
+;;
+;; constants
+;;
+
+(def DEFAULT-HOST "localhost")
+
+(def DEFAULT-PORT 8080)
