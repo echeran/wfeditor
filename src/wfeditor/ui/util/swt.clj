b0fda71ce109ddb7e0c2bdc10f710e7ad92b5688
Elango Cheran <elango.cheran@gmail.com>
2012-05-07 15:06:51 -0700

    found out about memfn, but still keep macro, added distinction to doc string

--- src/wfeditor/ui/util/swt.clj
+++ src/wfeditor/ui/util/swt.clj
@@ -16,54 +16,19 @@
    (isa? (class widget) org.eclipse.jface.window.Window) (.getShell widget)
    :else (when-let [parent (.getParent widget)] (get-ancestor-shell parent))))
 
-
-
-;; (defmacro fnify
-;;   "take the name of a Java member method and return a Clojure function that represents that Java method taking an object and optional other values as arguments"
-;;   [method-name]
-;;   `(fn [obj & args]
-;;     ~(into `(. obj ~method-name) `args)))
-
-;; this works
-;; (defmacro test3 [obj & args] `(.charAt ~obj ~@args))
-;; (test3 "hello world" 0)
-;; ;=> \h
-
-;; this works
-;; (defmacro test5 [method obj & args] `(. ~obj ~method ~@args))
-;; (test5 charAt "hello world" 0)
-;; ;=> \h
-
-;; almost works
-;; (defmacro test6 [method] `(fn [obj# & args#] (~concat (. obj# ~method) args#)))
-;; ((test6 charAt) "hello world" 0)
-;; ; IllegalArgumentException No matching field found: charAt for class java.lang.String  clojure.lang.Reflector.getInstanceField (Reflector.java:289)
-
-;; works, albeit potentially ugly
-;; (defmacro test7 [method] `(fn [obj# & args#] (eval (concat (list '. obj# '~method) args#))))
-;; ((test7 charAt) "hello world" 2)
-;; ;=> \l
-
-;; doesn't work
-;; (defmacro test8 [method] (fn [obj# & args#] (concat `(. obj# ~method) args#)))
-;; ((test8 charAt) "hello world" 2)
-;; ; IllegalArgumentException No matching ctor found for class user$test8$fn__1054  clojure.lang.Reflector.invokeConstructor (Reflector.java:166)
-
-;; TODO: figure out if this is the best way to achieve function-ifying
-;; a Java method
 (defmacro fnify
-  "take the name of a Java member method and return a Clojure function that represents that Java method taking an object and optional other values as arguments"
+  "take the name of a Java member method and return a Clojure function that represents that Java method taking an object and optional other values as arguments.  Use this when you do not want to be restricted by clojure.core/memfn to specifying the number of arguments that the function should accept upfront."
   [method-name]
   `(fn [obj# & args#]
-     (eval (concat (list '. obj# '~method) args#))))
+     (eval (concat (list '. obj# '~method-name) args#))))
 
 (defn update-button
   "update an SWT Button using a map of options. options map keys and values:
 :text  String of the button's text
 :select-fn  a fn of one argument (rep.'ing the selection event) that extends the SelectionAdapter and is given to the SelectionListener"
   [button opts]
-  (let [opts-fns-map {:text .setText :select-fn .addSelectionListener}
+  (let [opts-fns-map {:text (fnify setText) :select-fn (fnify addSelectionListener)}
         ;; opts-fns-map-fn (fn [k v] (if k (fn [b] ((opts-fns-map k) b v)) identity))
-        opts-fns-reduce-fn (f [b [k v]] (if k ((opts-fns-map k) b v) b))
+        opts-fns-reduce-fn (fn [b [k v]] (if k ((opts-fns-map k) b v) b))
         updated-button (reduce opts-fns-reduce-fn button opts-fns-map)]
     updated-button))
