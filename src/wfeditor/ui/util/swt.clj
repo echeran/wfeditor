a868774ae8dfaa9452386dfa1a263ad0bc56255c
Elango Cheran <elango.cheran@gmail.com>
2012-05-07 13:45:13 -0700

    adding function to update an SWT button, working on a macro to help generalize that

--- src/wfeditor/ui/util/swt.clj
+++ src/wfeditor/ui/util/swt.clj
@@ -15,3 +15,32 @@
    (isa? (class widget) org.eclipse.swt.widgets.Shell) widget
    (isa? (class widget) org.eclipse.jface.window.Window) (.getShell widget)
    :else (when-let [parent (.getParent widget)] (get-ancestor-shell parent))))
+
+
+
+;; (defmacro fnify
+;;   "take the name of a Java member method and return a Clojure function that represents that Java method taking an object and optional other values as arguments"
+;;   [method-name]
+;;   `(fn [obj & args]
+;;     ~(into `(. obj ~method-name) `args)))
+
+;; this works
+;; (defmacro test3 [obj & args] `(.charAt ~obj ~@args))
+;; (test3 "hello world" 0)
+;; ;=> \h
+
+;; this works
+;; (defmacro test5 [method obj & args] `(. ~obj ~method ~@args))
+;; (test5 charAt "hello world" 0)
+;; ;=> \h
+
+(defn update-button
+  "update an SWT Button using a map of options. options map keys and values:
+:text  String of the button's text
+:select-fn  a fn of one argument (rep.'ing the selection event) that extends the SelectionAdapter and is given to the SelectionListener"
+  [button opts]
+  (let [opts-fns-map {:text .setText :select-fn .addSelectionListener}
+        ;; opts-fns-map-fn (fn [k v] (if k (fn [b] ((opts-fns-map k) b v)) identity))
+        opts-fns-reduce-fn (f [b [k v]] (if k ((opts-fns-map k) b v) b))
+        updated-button (reduce opts-fns-reduce-fn button opts-fns-map)]
+    updated-button))
