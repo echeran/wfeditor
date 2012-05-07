f02939a2a6cc5a8a13831987b3b8a300bc2ac96c
Elango Cheran <elango.cheran@gmail.com>
2012-05-07 11:16:44 -0700

    did major refactoring and clean-up of GUI editor code

--- src/wfeditor/ui/gui/core.clj
+++ src/wfeditor/ui/gui/core.clj
@@ -1,216 +1,23 @@
 (ns wfeditor.ui.gui.core
-  (:gen-class)
   ;; putting the import statement in the ns form is the preferred way
   ;; of importing Java classes in Clojure (see
   ;; http://pragprog.com/magazines/2010-11/getting-clojure)
-  ;; (:require wfeditor.ui.gui.d2dcanvas)
   (:require wfeditor.ui.gui.zest.canvas
-            [wfeditor.model.workflow :as wflow]
-            [wfeditor.io.execution :as exec]
-            [wfeditor.io.file.wfeformat :as fformat]
-            [wfeditor.io.util.const :as io-const])
+            [wfeditor.ui.gui.editor-left :as editor-left])
   (:import
    org.eclipse.jface.window.ApplicationWindow
    org.eclipse.swt.SWT
-   (org.eclipse.swt.layout FillLayout FormLayout FormData FormAttachment GridLayout GridData RowLayout)
-   (org.eclipse.swt.widgets Display Shell Label Button Sash Composite FileDialog Group Text Combo)
-   (org.eclipse.swt.events SelectionEvent SelectionAdapter)
-   (org.eclipse.draw2d.geometry Rectangle Point)
-   ;; (org.eclipse.swt.graphics Rectangle Point) have to comment this
-   ;; out since it conflicts with the draw2d import statements
-   ;; according to Clojure compiler
-   ))
+   (org.eclipse.swt.layout FormLayout FormData FormAttachment GridLayout)
+   (org.eclipse.swt.widgets Display Shell Sash Composite)
+   (org.eclipse.swt.events SelectionEvent SelectionAdapter)))
 
-(defmacro new-widget [widget-class parent style]
-  `(do (new ~widget-class ~parent ~style)))
+;;
+;; functions
+;;
 
-(defmacro create-widgets-with-names [parent widget-class style names]
-  `(dorun (map #(.setText (new ~widget-class ~parent ~style) %1) ~names)))
-
-(defn create-buttons-with-names [parent  style names]
-  (dorun (map #(.setText (Button. parent style) %1) names)))
-
-
-;; TODO: move this function to a gui.util namespace
-;; TODO: also, create a one-stop, new widget convenience utility to end them all
-;; i that namespace
-(defn get-ancestor-shell
-  "return the ancestor shell of the provided widget / element, or else nil"
-  [widget]
-  (cond
-   (isa? (class widget) org.eclipse.swt.widgets.Shell) widget
-   (isa? (class widget) org.eclipse.jface.window.Window) (.getShell widget)
-   :else (when-let [parent (.getParent widget)] (get-ancestor-shell parent))))
-
-(defn ui-editor-left-create [parent]
-  (let [exec-group (new-widget Group parent SWT/SHADOW_ETCHED_IN)
-        user-label (new-widget Label exec-group SWT/LEFT)
-        user-text (new-widget Text exec-group (bit-or SWT/SINGLE SWT/BORDER))
-        exec-dom-label (new-widget Label exec-group SWT/LEFT)
-        exec-dom-combo (new-widget Combo exec-group (bit-or SWT/DROP_DOWN SWT/READ_ONLY))
-        rem-host-label (new-widget Label exec-group SWT/LEFT)
-        rem-host-text (new-widget Text exec-group (bit-or SWT/SINGLE SWT/BORDER))
-        rem-port-label (new-widget Label exec-group SWT/LEFT)
-        rem-port-text (new-widget Text exec-group (bit-or SWT/SINGLE SWT/BORDER))
-        loc-port-label (new-widget Label exec-group SWT/LEFT)
-        loc-port-text (new-widget Text exec-group (bit-or SWT/SINGLE SWT/BORDER))
-        button-group (new-widget Group parent SWT/SHADOW_NONE)
-        ;; label1 (new Label button-group SWT/CENTER)
-        print-wf-cmd-button (new-widget Button button-group SWT/PUSH)
-        print-wf-sge-test-button (new-widget Button button-group SWT/PUSH)
-        run-wf-button (new-widget Button button-group SWT/PUSH)
-        print-wf-button (new-widget Button button-group SWT/PUSH)
-        load-wf-button (new-widget Button button-group SWT/PUSH)
-        save-wf-button (new-widget Button button-group SWT/PUSH)
-        print-wf-inst-button (new-widget Button button-group SWT/PUSH)
-        update-wf-inst-button (new-widget Button button-group SWT/PUSH)
-        print-global-statuses-button (new-widget Button button-group SWT/PUSH)
-        testing-group (new-widget Group parent SWT/SHADOW_ETCHED_OUT)
-        label2 (Label. testing-group  SWT/CENTER)]
-    (do
-      ;; (.setLayout parent (RowLayout. SWT/VERTICAL))
-      (.setLayout parent (GridLayout.))
-      )
-    (doto exec-group
-      (.setText "Execution Properties")
-      (.setLayoutData (GridData. GridData/FILL_HORIZONTAL)))
-    (let [layout (GridLayout.)]
-      (do
-        (set! (. layout numColumns) 2)
-        (.setLayout exec-group layout)))
-    (doto user-label
-      (.setText "Enter username:")
-      (.setLayoutData (GridData. GridData/HORIZONTAL_ALIGN_BEGINNING)))
-    (doto user-text
-      (.setText (. System getProperty "user.name"))
-      (.setLayoutData (GridData. GridData/FILL_HORIZONTAL)))
-    (doto exec-dom-label
-      (.setText "Select execution domain:")
-      (.setLayoutData (GridData. GridData/HORIZONTAL_ALIGN_BEGINNING)))
-    (doto exec-dom-combo
-      (.add "SGE")
-      (.add "rem-piped-shell")
-      (.select 0)
-      (.setLayoutData (GridData. GridData/FILL_HORIZONTAL)))
-    (doto rem-host-label
-      (.setText "Remote host:")
-      (.setLayoutData (GridData. GridData/HORIZONTAL_ALIGN_BEGINNING)))
-    (doto rem-host-text
-      (.setText io-const/DEFAULT-HOST)
-      (.setLayoutData (GridData. GridData/FILL_HORIZONTAL)))
-    (doto rem-port-label
-      (.setText "Remote port:")
-      (.setLayoutData (GridData. GridData/HORIZONTAL_ALIGN_BEGINNING)))
-    (doto rem-port-text
-      (.setText (str io-const/DEFAULT-PORT))
-      (.setLayoutData (GridData. GridData/FILL_HORIZONTAL)))
-    (doto loc-port-label
-      (.setText "Local port:")
-      (.setLayoutData (GridData. GridData/HORIZONTAL_ALIGN_BEGINNING)))
-    (doto loc-port-text
-      (.setText (str io-const/DEFAULT-LOCAL-PORT))
-      (.setLayoutData (GridData. GridData/FILL_HORIZONTAL)))
-    (doto button-group
-      (.setLayout (FillLayout. SWT/VERTICAL))
-      (.setText "Buttons")
-      (.setLayoutData (GridData. GridData/FILL_BOTH)))
-    ;; (doto label1
-    ;;   (.setText "Working button(s)")
-    ;;   (.setBounds (.getClientArea parent)))
-    (doto print-wf-cmd-button
-      (.setText "Print workflow command")
-      (.addSelectionListener (proxy [SelectionAdapter]
-                                 []
-                               (widgetSelected [event]
-                                 (exec/print-wf-command (wflow/workflow))))))
-    (doto print-wf-sge-test-button
-      (.setText "Print SGE commands test")
-      (.addSelectionListener (proxy [SelectionAdapter]
-                                 []
-                               (widgetSelected [event]
-                                 (let [new-wf (wflow/wf-with-internal-ids (wflow/workflow))]
-                                   (exec/print-deps-in-order new-wf))))))
-    (doto run-wf-button
-      (.setText "Run workflow")
-      (.addSelectionListener (proxy [SelectionAdapter]
-                                 []
-                               (widgetSelected [event]                                 
-                                 (exec/run-workflow (wflow/workflow))))))
-    (doto print-wf-button
-      (.setText "Print workflow")
-      (.addSelectionListener (proxy [SelectionAdapter]
-                                 []
-                               (widgetSelected [event]
-                                 (println (fformat/workflow-to-string (wflow/workflow)))))))
-    (doto load-wf-button
-      (.setText "Load workflow")
-      (.addSelectionListener (proxy [SelectionAdapter]
-                                 []
-                               (widgetSelected [event]
-                                 (let [fd (new FileDialog (get-ancestor-shell parent) SWT/OPEN)]
-                                   (when-let [in-file-name (.open fd)]
-                                     (fformat/set-workflow-from-file in-file-name)))))))
-    (doto save-wf-button
-      (.setText "Save workflow")
-      (.addSelectionListener (proxy [SelectionAdapter]
-                                 []
-                               (widgetSelected [event]
-                                 (let [fd (FileDialog. (get-ancestor-shell parent) SWT/SAVE)]
-                                   (when-let [out-file-name (.open fd)]
-                                     (fformat/save-workflow-to-file (wflow/workflow) out-file-name)))))))
-    (doto print-wf-inst-button
-      (.setText "Print WF instance")
-      (.addSelectionListener (proxy [SelectionAdapter]
-                                 []
-                               (widgetSelected [event]
-                                 (let [username (.getText user-text)
-                                       workflow (wflow/workflow)
-                                       exec-domain (.getItem exec-dom-combo (.getSelectionIndex exec-dom-combo))
-                                       wf-inst (wflow/new-wfinstance-fn username exec-domain workflow)
-                                       wf-inst-str (fformat/workflow-instance-to-string wf-inst)]
-                                   (println wf-inst-str))))))
-    (doto update-wf-inst-button
-      (.setText "Update WF instance via server")
-      (.addSelectionListener (proxy [SelectionAdapter]
-                                 []
-                               (widgetSelected [event]
-                                 (let [username (.getText user-text)
-                                       workflow (wflow/workflow)
-                                       exec-domain (.getItem exec-dom-combo (.getSelectionIndex exec-dom-combo))
-                                       wf-inst (wflow/new-wfinstance-fn username exec-domain workflow)
-                                       rem-host (.getText rem-host-text)
-                                       rem-port (Integer/parseInt (.getText rem-port-text))
-                                       loc-port (Integer/parseInt (.getText loc-port-text))
-                                       loc-host io-const/DEFAULT-LOCAL-HOST
-                                       server-host io-const/DEFAULT-SERVER-HOST-REL-TO-REMOTE
-                                       ]
-                                   ;; (exec/update-wfinst-and-set-everywhere wf-inst)
-                                   (exec/update-wfinst-and-set-everywhere wf-inst rem-host rem-port loc-port loc-host server-host)                                   )))))
-    (doto print-global-statuses-button
-      (.setText "Print global statuses")
-      (.addSelectionListener (proxy [SelectionAdapter]
-                                 []
-                               (widgetSelected [event]
-                                 (println (exec/global-statuses))))))
-    (doto testing-group
-      (.setText "Testing")
-      (.setLayout (RowLayout. SWT/VERTICAL))
-      (.setLayoutData (GridData. GridData/HORIZONTAL_ALIGN_BEGINNING)))
-    (doto label2
-      (.setText "Testing/non-working button(s)"))
-    (create-widgets-with-names testing-group Button SWT/PUSH ["one" "two" "three"])
-    (do
-      (create-widgets-with-names testing-group Button SWT/RADIO ["Radio 1" "Radio 2" "Radio 3"])
-      (create-widgets-with-names testing-group Button SWT/TOGGLE ["Tog 1" "Tog 2" "Tog 3"])
-      (create-widgets-with-names testing-group Button SWT/CHECK [ "Check one" "...two" "...three"]))))
-
-(defn ui-editor-right-create [parent]
-  (let []
-    
-    ;; (wfeditor.ui.gui.d2dcanvas/create-diagram parent)
-    (wfeditor.ui.gui.zest.canvas/graph-viewer-create parent)
-
-    (.setLayout parent (GridLayout.))))
+(defn ui-editor-right-create [parent] 
+  (wfeditor.ui.gui.zest.canvas/graph-viewer-create parent) 
+  (.setLayout parent (GridLayout.)))
 
 ;; assume FormLayout of the parent widget to which the returned sash
 ;; will be attached
@@ -225,20 +32,17 @@
     sash))
 
 (defn ui-editor-create [parent]
-  (let [
-        sash (Sash. parent (bit-or SWT/VERTICAL SWT/BORDER SWT/SMOOTH))
+  (let [sash (Sash. parent (bit-or SWT/VERTICAL SWT/BORDER SWT/SMOOTH))
         ;; have to make the style of the elements next to Sash have
         ;; BORDER so that Sash is drawn identifiably
         comp-left (Composite. parent SWT/BORDER)
         comp-right (Composite. parent SWT/BORDER)
-        ;; sash (create-sash parent comp-left comp-right)
         sash-fdata (FormData.)
         comp-left-fdata (FormData.)
-        comp-right-fdata (FormData.)
-        ]
+        comp-right-fdata (FormData.)]
     (do
       (.setLayout parent (FormLayout.))
-      (ui-editor-left-create comp-left)
+      (editor-left/ui-editor-left-create comp-left)
       (ui-editor-right-create comp-right))
     (do
       (set! (. sash-fdata top) (FormAttachment. 0 0))
---
+++ src/wfeditor/ui/gui/editor_left.clj
@@ -0,0 +1,186 @@
+(ns wfeditor.ui.gui.editor-left
+  (:require [wfeditor.io.util.const :as io-const]
+            [wfeditor.model.workflow :as wflow]
+            [wfeditor.io.execution :as exec]
+            [wfeditor.io.file.wfeformat :as fformat])
+  (:import
+   org.eclipse.jface.window.ApplicationWindow
+   org.eclipse.swt.SWT
+   (org.eclipse.swt.layout FillLayout RowLayout GridLayout GridData)
+   (org.eclipse.swt.widgets Shell Label Button FileDialog Group Text Combo)
+   (org.eclipse.swt.events SelectionEvent SelectionAdapter)))
+
+;;
+;; functions
+;;
+
+(defmacro new-widget [widget-class parent style]
+  `(do (new ~widget-class ~parent ~style)))
+
+(defmacro create-widgets-with-names [parent widget-class style names]
+  `(dorun (map #(.setText (new ~widget-class ~parent ~style) %1) ~names)))
+
+;; TODO: move this function to a gui.util namespace
+;; TODO: also, create a one-stop, new widget convenience utility to end them all
+;; i that namespace
+(defn get-ancestor-shell
+  "return the ancestor shell of the provided widget / element, or else nil"
+  [widget]
+  (cond
+   (isa? (class widget) org.eclipse.swt.widgets.Shell) widget
+   (isa? (class widget) org.eclipse.jface.window.Window) (.getShell widget)
+   :else (when-let [parent (.getParent widget)] (get-ancestor-shell parent))))
+
+(defn ui-editor-left-create [parent]
+  (let [exec-group (new-widget Group parent SWT/SHADOW_ETCHED_IN)
+        user-label (new-widget Label exec-group SWT/LEFT)
+        user-text (new-widget Text exec-group (bit-or SWT/SINGLE SWT/BORDER))
+        exec-dom-label (new-widget Label exec-group SWT/LEFT)
+        exec-dom-combo (new-widget Combo exec-group (bit-or SWT/DROP_DOWN SWT/READ_ONLY))
+        rem-host-label (new-widget Label exec-group SWT/LEFT)
+        rem-host-text (new-widget Text exec-group (bit-or SWT/SINGLE SWT/BORDER))
+        rem-port-label (new-widget Label exec-group SWT/LEFT)
+        rem-port-text (new-widget Text exec-group (bit-or SWT/SINGLE SWT/BORDER))
+        loc-port-label (new-widget Label exec-group SWT/LEFT)
+        loc-port-text (new-widget Text exec-group (bit-or SWT/SINGLE SWT/BORDER))
+        button-group (new-widget Group parent SWT/SHADOW_NONE)
+        print-wf-cmd-button (new-widget Button button-group SWT/PUSH)
+        print-wf-sge-test-button (new-widget Button button-group SWT/PUSH)
+        run-wf-button (new-widget Button button-group SWT/PUSH)
+        print-wf-button (new-widget Button button-group SWT/PUSH)
+        load-wf-button (new-widget Button button-group SWT/PUSH)
+        save-wf-button (new-widget Button button-group SWT/PUSH)
+        print-wf-inst-button (new-widget Button button-group SWT/PUSH)
+        update-wf-inst-button (new-widget Button button-group SWT/PUSH)
+        print-global-statuses-button (new-widget Button button-group SWT/PUSH)
+        testing-group (new-widget Group parent SWT/SHADOW_ETCHED_OUT)
+        label2 (Label. testing-group  SWT/CENTER)]
+    (do
+      (.setLayout parent (GridLayout.)))
+    (doto exec-group
+      (.setText "Execution Properties")
+      (.setLayoutData (GridData. GridData/FILL_HORIZONTAL)))
+    (let [layout (GridLayout.)]
+      (do
+        (set! (. layout numColumns) 2)
+        (.setLayout exec-group layout)))
+    (doto user-label
+      (.setText "Enter username:")
+      (.setLayoutData (GridData. GridData/HORIZONTAL_ALIGN_BEGINNING)))
+    (doto user-text
+      (.setText (. System getProperty "user.name"))
+      (.setLayoutData (GridData. GridData/FILL_HORIZONTAL)))
+    (doto exec-dom-label
+      (.setText "Select execution domain:")
+      (.setLayoutData (GridData. GridData/HORIZONTAL_ALIGN_BEGINNING)))
+    (doto exec-dom-combo
+      (.add "SGE")
+      (.add "rem-piped-shell")
+      (.select 0)
+      (.setLayoutData (GridData. GridData/FILL_HORIZONTAL)))
+    (doto rem-host-label
+      (.setText "Remote host:")
+      (.setLayoutData (GridData. GridData/HORIZONTAL_ALIGN_BEGINNING)))
+    (doto rem-host-text
+      (.setText io-const/DEFAULT-HOST)
+      (.setLayoutData (GridData. GridData/FILL_HORIZONTAL)))
+    (doto rem-port-label
+      (.setText "Remote port:")
+      (.setLayoutData (GridData. GridData/HORIZONTAL_ALIGN_BEGINNING)))
+    (doto rem-port-text
+      (.setText (str io-const/DEFAULT-PORT))
+      (.setLayoutData (GridData. GridData/FILL_HORIZONTAL)))
+    (doto loc-port-label
+      (.setText "Local port:")
+      (.setLayoutData (GridData. GridData/HORIZONTAL_ALIGN_BEGINNING)))
+    (doto loc-port-text
+      (.setText (str io-const/DEFAULT-LOCAL-PORT))
+      (.setLayoutData (GridData. GridData/FILL_HORIZONTAL)))
+    (doto button-group
+      (.setLayout (FillLayout. SWT/VERTICAL))
+      (.setText "Buttons")
+      (.setLayoutData (GridData. GridData/FILL_BOTH)))
+    (doto print-wf-cmd-button
+      (.setText "Print workflow command")
+      (.addSelectionListener (proxy [SelectionAdapter]
+                                 []
+                               (widgetSelected [event]
+                                 (exec/print-wf-command (wflow/workflow))))))
+    (doto print-wf-sge-test-button
+      (.setText "Print SGE commands test")
+      (.addSelectionListener (proxy [SelectionAdapter]
+                                 []
+                               (widgetSelected [event]
+                                 (let [new-wf (wflow/wf-with-internal-ids (wflow/workflow))]
+                                   (exec/print-deps-in-order new-wf))))))
+    (doto run-wf-button
+      (.setText "Run workflow")
+      (.addSelectionListener (proxy [SelectionAdapter]
+                                 []
+                               (widgetSelected [event]                                 
+                                 (exec/run-workflow (wflow/workflow))))))
+    (doto print-wf-button
+      (.setText "Print workflow")
+      (.addSelectionListener (proxy [SelectionAdapter]
+                                 []
+                               (widgetSelected [event]
+                                 (println (fformat/workflow-to-string (wflow/workflow)))))))
+    (doto load-wf-button
+      (.setText "Load workflow")
+      (.addSelectionListener (proxy [SelectionAdapter]
+                                 []
+                               (widgetSelected [event]
+                                 (let [fd (new FileDialog (get-ancestor-shell parent) SWT/OPEN)]
+                                   (when-let [in-file-name (.open fd)]
+                                     (fformat/set-workflow-from-file in-file-name)))))))
+    (doto save-wf-button
+      (.setText "Save workflow")
+      (.addSelectionListener (proxy [SelectionAdapter]
+                                 []
+                               (widgetSelected [event]
+                                 (let [fd (FileDialog. (get-ancestor-shell parent) SWT/SAVE)]
+                                   (when-let [out-file-name (.open fd)]
+                                     (fformat/save-workflow-to-file (wflow/workflow) out-file-name)))))))
+    (doto print-wf-inst-button
+      (.setText "Print WF instance")
+      (.addSelectionListener (proxy [SelectionAdapter]
+                                 []
+                               (widgetSelected [event]
+                                 (let [username (.getText user-text)
+                                       workflow (wflow/workflow)
+                                       exec-domain (.getItem exec-dom-combo (.getSelectionIndex exec-dom-combo))
+                                       wf-inst (wflow/new-wfinstance-fn username exec-domain workflow)
+                                       wf-inst-str (fformat/workflow-instance-to-string wf-inst)]
+                                   (println wf-inst-str))))))
+    (doto update-wf-inst-button
+      (.setText "Update WF instance via server")
+      (.addSelectionListener (proxy [SelectionAdapter]
+                                 []
+                               (widgetSelected [event]
+                                 (let [username (.getText user-text)
+                                       workflow (wflow/workflow)
+                                       exec-domain (.getItem exec-dom-combo (.getSelectionIndex exec-dom-combo))
+                                       wf-inst (wflow/new-wfinstance-fn username exec-domain workflow)
+                                       rem-host (.getText rem-host-text)
+                                       rem-port (Integer/parseInt (.getText rem-port-text))
+                                       loc-port (Integer/parseInt (.getText loc-port-text))
+                                       loc-host io-const/DEFAULT-LOCAL-HOST
+                                       server-host io-const/DEFAULT-SERVER-HOST-REL-TO-REMOTE]
+                                   (exec/update-wfinst-and-set-everywhere wf-inst rem-host rem-port loc-port loc-host server-host)                                   )))))
+    (doto print-global-statuses-button
+      (.setText "Print global statuses")
+      (.addSelectionListener (proxy [SelectionAdapter]
+                                 []
+                               (widgetSelected [event]
+                                 (println (exec/global-statuses))))))
+    (doto testing-group
+      (.setText "Testing")
+      (.setLayout (RowLayout. SWT/VERTICAL))
+      (.setLayoutData (GridData. GridData/HORIZONTAL_ALIGN_BEGINNING)))
+    (doto label2
+      (.setText "Testing/non-working button(s)"))
+    (create-widgets-with-names testing-group Button SWT/PUSH ["one" "two" "three"])
+    (do
+      (create-widgets-with-names testing-group Button SWT/RADIO ["Radio 1" "Radio 2" "Radio 3"])
+      (create-widgets-with-names testing-group Button SWT/TOGGLE ["Tog 1" "Tog 2" "Tog 3"])
+      (create-widgets-with-names testing-group Button SWT/CHECK [ "Check one" "...two" "...three"]))))
